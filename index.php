4e160819365d9ba251c286471c7d0c4cd4f19732
ecolindres1997 <60886570+ecolindres1997@users.noreply.github.com>
2020-05-26 10:08:48 -0600

    version1

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Agenda DIPESCA</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdn.timekit.io/booking-js/v2/booking.min.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/locale/es.js" defer></script>
+  <link rel="icon" type="image/png" href="maga.ico">
+  <script src="js.js"></script>>
+ </head>
+ <body>
+  <br />
+  <br />
+  <div class="container">
+   <div id="calendar"></div>
+  </div>
+ </body>
+</html>
---
+++ prueba.php
@@ -0,0 +1,16 @@
+<div id="bookingjs"></div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdn.timekit.io/booking-js/v2/booking.min.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/locale/es.js" defer></script>
+<script type="text/javascript">
+  window.timekitBookingConfig = {
+    app_key: 'live_widget_key_yYXHsUurvCUctNYflzGDMqEJo6mol0Yo',
+    resources: [],
+    fullcalendar: {
+      locale: 'es'
+    },
+    ui: {
+      time_date_format: false
+    }
+  };
+</script>
